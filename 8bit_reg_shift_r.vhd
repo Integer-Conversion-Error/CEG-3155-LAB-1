47414e4a8d37a524ce956a808615abb88fc54de8
Integer-Conversion-Error <122304021+Integer-Conversion-Error@users.noreply.github.com>
2024-09-26 16:44:27 -0400

    fixed right shift register(?)

--- 8bit_reg_shift_r.vhd
+++ 8bit_reg_shift_r.vhd
@@ -1,76 +1,35 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity d_flip_flop is
+-- 8-bit Register entity
+entity Register8bitr is
     Port (
-        clk    : in  STD_LOGIC;
-        reset  : in  STD_LOGIC;
-        d      : in  STD_LOGIC;
-        q      : out STD_LOGIC
+        CLK   : in  STD_LOGIC;                  -- Clock input
+        LD    : in  STD_LOGIC;                  -- Load signal input
+		  SHR	  : in STD_LOGIC;							-- Shift Right input
+        D_in  : in  STD_LOGIC_VECTOR (7 downto 0);  -- 8-bit Data input
+        Q_out : out STD_LOGIC_VECTOR (7 downto 0)   -- 8-bit Data output
     );
-end d_flip_flop;
+end Register8bitr;
 
-architecture Behavioral of d_flip_flop is
-begin
-    process (clk, reset)
-    begin
-        if reset = '1' then
-            q <= '0';
-        elsif rising_edge(clk) then
-            q <= d;
-        end if;
-    end process;
-end Behavioral;
-
-
-entity shift_register is
-    Port (
-        clk         : in  STD_LOGIC;          
-        reset       : in  STD_LOGIC;          
-        load        : in  STD_LOGIC;          
-        data_in     : in  STD_LOGIC_VECTOR(7 downto 0); 
-        shift_right : in  STD_LOGIC;          
-        q           : out STD_LOGIC_VECTOR(7 downto 0)  
-    );
-end shift_register;
-
-architecture Structural of shift_register is
-    signal d, q_internal : STD_LOGIC_VECTOR(7 downto 0);
-    signal load_or_shift : STD_LOGIC_VECTOR(7 downto 0);
-
-    component d_flip_flop
-        Port (
-            clk   : in  STD_LOGIC;
-            reset : in  STD_LOGIC;
-            d     : in  STD_LOGIC;
-            q     : out STD_LOGIC
-        );
-    end component;
+architecture structural of Register8bitr is
+    -- Internal signals for the outputs of each D flip-flop
+    signal Q : STD_LOGIC_VECTOR (7 downto 0);
+	 constant EndBit : integer := 0;
 
 begin
 
-    process (data_in, q_internal, load, shift_right)
-    begin
-        if load = '1' then
-            load_or_shift <= data_in;
-        elsif shift_right = '1' then
-            load_or_shift(7) <= '0';          
-            load_or_shift(6 downto 0) <= q_internal(7 downto 1);  
-        else
-            load_or_shift <= q_internal;      
-        end if;
-    end process;
+    -- Instantiating 8 D flip-flops to form an 8-bit register
+    U0: entity work.DFF port map (i_d => D_in(0), i_clock => CLK, o_q => (Q(0) AND NOT(SHR)) OR (Q(1) AND SHR));
+    U1: entity work.DFF port map (i_d => D_in(1), i_clock => CLK, o_q => (Q(1) AND NOT(SHR)) OR (Q(2) AND SHR));
+    U2: entity work.DFF port map (i_d => D_in(2), i_clock => CLK, o_q => (Q(2) AND NOT(SHR)) OR (Q(3) AND SHR));
+    U3: entity work.DFF port map (i_d => D_in(3), i_clock => CLK, o_q => (Q(3) AND NOT(SHR)) OR (Q(4) AND SHR));
+    U4: entity work.DFF port map (i_d => D_in(4), i_clock => CLK, o_q => (Q(4) AND NOT(SHR)) OR (Q(5) AND SHR));
+    U5: entity work.DFF port map (i_d => D_in(5), i_clock => CLK, o_q => (Q(5) AND NOT(SHR)) OR (Q(6) AND SHR));
+    U6: entity work.DFF port map (i_d => D_in(6), i_clock => CLK, o_q => (Q(6) AND NOT(SHR)) OR (Q(7) AND SHR));
+    U7: entity work.DFF port map (i_d => D_in(7), i_clock => CLK, o_q => (Q(7) AND NOT(SHR)) OR (EndBit AND SHR));
 
-    U0: d_flip_flop port map(clk, reset, load_or_shift(0), q_internal(0));
-    U1: d_flip_flop port map(clk, reset, load_or_shift(1), q_internal(1));
-    U2: d_flip_flop port map(clk, reset, load_or_shift(2), q_internal(2));
-    U3: d_flip_flop port map(clk, reset, load_or_shift(3), q_internal(3));
-    U4: d_flip_flop port map(clk, reset, load_or_shift(4), q_internal(4));
-    U5: d_flip_flop port map(clk, reset, load_or_shift(5), q_internal(5));
-    U6: d_flip_flop port map(clk, reset, load_or_shift(6), q_internal(6));
-    U7: d_flip_flop port map(clk, reset, load_or_shift(7), q_internal(7));
+    -- Connect the internal signal Q to the output of the register
+    Q_out <= Q;
 
-    q <= q_internal;
-end Structural;
+end structural;
