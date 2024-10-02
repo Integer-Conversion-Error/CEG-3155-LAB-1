38a320675170478fcb46dedb6c4b420bea10dba0
Integer-Conversion-Error <122304021+Integer-Conversion-Error@users.noreply.github.com>
2024-10-01 20:11:57 -0400

    fixing top level

--- 2_4_mux.vhd
+++ 2_4_mux.vhd
@@ -3,15 +3,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux4x1 is
     Port ( 
-        I0, I1, I2, I3 : in STD_LOGIC;  
+        I0, I1, I2, I3 : in STD_LOGIC_VECTOR(7 downto 0);  
         S0, S1         : in STD_LOGIC;  
-        Y              : out STD_LOGIC  
+        Y              : out STD_LOGIC_VECTOR(7 downto 0); 
     );
 end mux4x1;
 
 architecture structural of mux4x1 is
 
-    signal a0, a1, a2, a3 : STD_LOGIC;
+    signal a0, a1, a2, a3 : STD_LOGIC_VECTOR(7 downto 0);
 
 begin
 
